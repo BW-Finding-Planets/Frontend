cc1f6c09164aebce72c41e5c36bbbf4d49d2e632
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-08-26 18:56:10 -0400

    added a login page using formik, also using components built but materialui

---
+++ finding-planets/src/state/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+name: '',
+
+}
+
+ console.log('name', state)
+export const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return{...state,   }
+
+
+  }
+}
