ab8ff91bdfb658f2751cf80136d5fe0ac84f8b88
Your Name <you@example.com>
2019-08-29 16:31:41 -0400

    done styling for my profile

--- finding-planets/src/components/MyProfile.js
+++ finding-planets/src/components/MyProfile.js
@@ -1,9 +1,47 @@
 import React, {useEffect, useState} from 'react';
 import Axios from 'axios';
+import Card from '@material-ui/core/Card';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import MenuAppBar from './MenuAppBar';
+import './MyProfile.css'
+
+
+
+const useStyles = makeStyles({
+    card: {
+      maxWidth: 500,
+      display: 'flex',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      flexWrap: 'wrap',
+      marginTop: '220px',
+      width: '400px',
+    },
+    media: {
+      height: 200,
+      paddingTop: '56.25%', // 16:9
+      
+    },
+    btn:
+    {
+    marginTop: '30px',
+    background: 'orange',
+    // marginBottom: '30px',
+
+    },
+    error:{
+      color: 'red',
+      fontSize: '.7rem'
+
+    }
+})
+
 
 const MyProfile = (props) => {
     console.log('props',props)
     const [displayProfile, setDisplayProfile] = useState({firstName:'', lastName:'', email:'', profession:''})
+    const classes = useStyles();
 
     const id = props.userId
 
@@ -23,18 +61,32 @@ const MyProfile = (props) => {
     }, [])
 
     return (
-        <div>
-            <p>Username: {displayProfile.username}</p>
-            <p>Name: {displayProfile.firstName} {displayProfile.lastName}</p>
-            <p>Email: {displayProfile.email}</p>
-            <p>Profession: {displayProfile.profession}</p>
-            <button onClick={
-                // ()=> props.history.push('/AppPage')
-                () => {if (displayProfile.firstName) {return props.history.push('/AppPage')} 
-                else {return props.history.push('/createprofile')}
-                }
-                }
-                >Next</button>
+        <div className="container">
+            <Card className={classes.card}>
+                
+                <h1>{displayProfile.firstName} Profile</h1>
+                <div className="myProfile">
+                    <img 
+                        src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200" 
+                        alt="User Profile" height="200" width="200"/>
+                    <div className="user-info">
+                        <p>Username: {displayProfile.username}</p>
+                        <p>Name: {displayProfile.firstName} {displayProfile.lastName}</p>
+                        <p>Email: {displayProfile.email}</p>
+                        <p>Profession: {displayProfile.profession}</p>
+                    </div>
+                    <Button 
+                        className={classes.btn}
+                        onClick={
+                        // ()=> props.history.push('/AppPage')
+                        () => {if (displayProfile.firstName) {return props.history.push('/AppPage')} 
+                        else {return props.history.push('/createprofile')}
+                        }
+                        }
+                    >Next</Button>
+
+                </div>
+            </Card>
         </div>
     )
 }
