11c2923b1ca55e83ed44736adbc2fa0e39224e21
Heorhii Siburov <g.siburov@gmail.com>
2019-08-29 16:17:57 -0700

    axios .put finally works!

--- finding-planets/src/components/RatingStar.js
+++ finding-planets/src/components/RatingStar.js
@@ -1,36 +1,28 @@
 import React, { Component, useEffect } from 'react'
 import { Rating } from 'semantic-ui-react'
-
+import axios from "axios";
 
 export default class RatingExampleOnRate extends Component {
-  state = {}
 
-  handleRate = (e, { rating, maxRating }) => {
+
+  handleRate = (e, { rating }) => {
   console.log("Given rating is: ", rating)
-  // console.log("Maxrating is: ", maxRating)
 
-  // useEffect(() => {
+  axios
+    .put(`https://finding-planets.herokuapp.com/candidate/3/veryLikely`, {veryLikely: rating},  {
+      headers: { Authorization: localStorage.getItem("token") }
+    })
 
-//   axios
-//     .put(`https://finding-planets.herokuapp.com/candidate/:id/veryLikely`, {
-//       headers: { Authorization: localStorage.getItem("token") }
-//     })
-//     .then(response => {
-//       const starData = response.data;
-//       console.log("Star data is here", starData);
-//       setStarobj(starData);
-//       console.log(token)
-//     })
-//     .catch(function (error) {
-//         console.log("Oh-oh, something wrong with main array", error);
-//     });
-// }, []);
-
-
-
+    .then(response => {
+      console.log("Vote data is here", response);
+    })
+    .catch(function (error) {
+        console.log("rating inside", rating);
+        console.log("Oh-oh, something wrong with sending of vote", error);
+    });
 
   }
-  // this.setState({ rating, maxRating })
+
     
     
   render() {
@@ -38,7 +30,6 @@ export default class RatingExampleOnRate extends Component {
     return (
       <div>
         <Rating maxRating={5} onRate={this.handleRate} />
-        {/* <pre>{JSON.stringify(this.state, null, 2)}</pre> */}
       </div>
     )
   }
