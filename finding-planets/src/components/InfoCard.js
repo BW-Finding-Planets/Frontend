305425d0b5cf2e265fb37335d18cedccc1ff7b30
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-08-29 23:08:40 -0400

    star curve images are working

--- finding-planets/src/components/InfoCard.js
+++ finding-planets/src/components/InfoCard.js
@@ -4,8 +4,8 @@ import { Image, Item, Dimmer, Loader, Segment } from 'semantic-ui-react'
 
 
 const StarCard = props => {
-     
-    if (!props.id) return <div>    
+     console.log("InfoCard", props)
+    if (!props.id) return <div>
         <Segment>
             <Dimmer active inverted>
                 <Loader inverted>Loading</Loader>
@@ -18,8 +18,8 @@ const StarCard = props => {
     return (
         <Item.Group >
         <Item>
-         
-           
+
+
           <Item.Content>
             <Item.Header>Star {props.ticid_x}</Item.Header>
             <Item.Meta>
@@ -27,22 +27,22 @@ const StarCard = props => {
               <span className='stay'>{props.star_radius}</span>
             </Item.Meta>
             <p>Insolation Flux: {props.InsolationFlux}</p>
-            <p>magnitude: {props.magnitude}</p>   
-            <p>luminosity: {props.luminosity}</p>     
-            <p>star_mass: {props.star_mass}</p>     
-            <p>star_radius: {props.star_radius}</p>     
-            <p>constellation: {props.constellation}</p>     
-            <p>rightascension: {props.rightascension}</p>     
-            <p>declination: {props.declination}</p>   
-            <p>starTeffKelvin: {props.starTeffKelvin}</p>   
-            <p>model_prediction: {props.predictions}</p>   
+            <p>magnitude: {props.magnitude}</p>
+            <p>luminosity: {props.luminosity}</p>
+            <p>star_mass: {props.star_mass}</p>
+            <p>star_radius: {props.star_radius}</p>
+            <p>constellation: {props.constellation}</p>
+            <p>rightascension: {props.rightascension}</p>
+            <p>declination: {props.declination}</p>
+            <p>starTeffKelvin: {props.starTeffKelvin}</p>
+            <p>model_prediction: {props.predictions}</p>
             <p>distance: {props.distance}</p>
           </Item.Content>
         </Item>
-    
+
       </Item.Group>
-    
+
     );
   };
-  
+
   export default StarCard;
